bbea9a1b8cd70a37394388e6369770f43548b11c
Abhishek Rana <93991456+abhirana2001@users.noreply.github.com>
2025-06-30 12:17:10 +0530

    Update axiosInstance.js

--- frontend/src/features/axiosInstance.js
+++ frontend/src/features/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://hrmsdashboard-2ga6.onrender.com",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
